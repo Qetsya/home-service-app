7df47e013340dc1494bd18d1662f397373e9f234
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-08-29 18:30:12 +0300

    export const BusinessPage: React.FC added

--- frontend/src/components/pages/SinglePage/BusinessPage.tsx
+++ frontend/src/components/pages/SinglePage/BusinessPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 
-export const BusinessPage = () => {
+export const BusinessPage: React.FC = () => {
   const { business } = useParams<{ business: string }>();
   return (
     <div>
