83ba25106881f5fe3505bbae84f0e8131f3aa341
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-08-29 19:29:31 +0300

    creating layout for BusinessPage

--- frontend/src/components/pages/SinglePage/BusinessPage.tsx
+++ frontend/src/components/pages/SinglePage/BusinessPage.tsx
@@ -1,11 +1,17 @@
 import { useParams } from 'react-router-dom';
+import styles from './BusinessPage.module.scss';
 
 export const BusinessPage: React.FC = () => {
   const { business } = useParams<{ business: string }>();
   return (
     <div>
-      <h1>Business Page for {business}</h1>
-      <div className="detail">yo</div>
+      <h1 className={styles.title}>Business Page for {business}</h1>
+      <div className={styles.gridContainer}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
     </div>
   );
 };
