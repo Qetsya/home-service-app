231127ed1269b42ea2964408c26bc5997567007e
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-09-02 17:02:58 +0300

    description card created

--- frontend/src/components/pages/SinglePage/BusinessPage.tsx
+++ frontend/src/components/pages/SinglePage/BusinessPage.tsx
@@ -24,7 +24,16 @@ export const BusinessPage: React.FC = () => {
           <p className={styles.avaiName}>Jenny Wilson</p>
           <p className={styles.bookings}>Available 8:00 AM to 10PM</p>
         </div>
-        <div>3</div>
+        <div className="descriptionCard">
+          <h3 className={styles.descriptionName}>Description</h3>
+          <div className={styles.descriptionText}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla provident ipsum placeat aspernatur est
+            accusamus architecto modi deleniti, ipsa vero voluptatem assumenda beatae vel illo quibusdam facere tenetur
+            delectus suscipit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa reiciendis aliquam,
+            excepturi enim ad alias quod corrupti quasi nulla blanditiis esse ab provident aliquid accusamus facilis
+            quas fuga! Animi, voluptatibus.
+          </div>
+        </div>
         <div>4</div>
       </div>
     </div>
