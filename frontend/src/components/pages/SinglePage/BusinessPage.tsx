db64547e8543c03dcc30a3fa193ee3e96a539022
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-09-02 16:13:36 +0300

    styling infoCard

--- frontend/src/components/pages/SinglePage/BusinessPage.tsx
+++ frontend/src/components/pages/SinglePage/BusinessPage.tsx
@@ -5,17 +5,17 @@ export const BusinessPage: React.FC = () => {
   return (
     <div>
       <div className={styles.gridContainer}>
-        <div className={styles.wrapper}>
+        <div className={styles.infoCard}>
           <img
             src="https://www.pristinehome.com.au/wp-content/uploads/2020/01/15-Cleaning-Tips-from-Professional-Cleaners-3.jpg"
             className={styles.pageImage}
           />
           <div className={styles.info}>
-            <button className={styles.button}>cleaning</button>
+            <div className={styles.button}>cleaning</div>
             <div className={styles.businessTag}></div>
-            <h4>House Cleaning</h4>
-            <p>255 Grand Park Ave, New York</p>
-            <p>accounts@tubeguruji.com</p>
+            <h3 className={styles.pageTitle}>House Cleaning</h3>
+            <p className={styles.address}>255 Grand Park Ave, New York</p>
+            <p className={styles.email}>accounts@tubeguruji.com</p>
           </div>
         </div>
         <div>2</div>
