c24633ea75259ad16cca3dba1fe1327351c0f65c
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-09-02 16:52:16 +0300

    grid 2 in progress, button, and text styling

--- frontend/src/components/pages/SinglePage/BusinessPage.tsx
+++ frontend/src/components/pages/SinglePage/BusinessPage.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import styles from './BusinessPage.module.scss';
+import { Fa } from 'react-icons/fa';
 
 export const BusinessPage: React.FC = () => {
   return (
@@ -18,7 +19,11 @@ export const BusinessPage: React.FC = () => {
             <p className={styles.email}>accounts@tubeguruji.com</p>
           </div>
         </div>
-        <div>2</div>
+        <div className={styles.availableTab}>
+          <button className={styles.bookingBox}>Box</button>
+          <p className={styles.avaiName}>Jenny Wilson</p>
+          <p className={styles.bookings}>Available 8:00 AM to 10PM</p>
+        </div>
         <div>3</div>
         <div>4</div>
       </div>
