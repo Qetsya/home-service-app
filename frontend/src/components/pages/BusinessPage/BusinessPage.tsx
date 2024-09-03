9e658e50010b86289e1c7ca48c82179b800f1094
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-09-03 17:27:55 +0300

    gradient bg added

--- frontend/src/components/pages/BusinessPage/BusinessPage.tsx
+++ frontend/src/components/pages/BusinessPage/BusinessPage.tsx
@@ -13,20 +13,20 @@ export const BusinessPage = () => {
   return (
     <>
       <BookingCalendar onOpen={open} setClose={handleClose} />
-      <div className="grid grid-cols-[60%,40%]  gap-2 p-2 m-[5rem_5rem] grid-rows-[fit-content(200px)] w-4/5;">
+      <div className="grid grid-cols-[65%,35%] p-2 m-[5rem_5rem] grid-rows-[fit-content(200px) ">
         <div className="flex justify-left gap-7 ;">
           <img
             src="https://www.pristinehome.com.au/wp-content/uploads/2020/01/15-Cleaning-Tips-from-Professional-Cleaners-3.jpg"
-            className="h-[150px] w-[150px] rounded-full object-cover;"
+            className="h-[150px] w-[150px] rounded-full object-cover ml-10 pl-10;"
           />
-          <div className="flex justify-start flex-col items-start gap-10;">
-            <div className=" py-1 px-3 rounded-full border-none cursor-pointer text-[#8056eb] bg-[#e5ddfb] text-sm w-20 text-center;">
+          <div className="flex justify-start flex-col items-start gap-20;">
+            <div className=" py-1 px-3 rounded-full border-none cursor-pointer text-[#8056eb] bg-[#e5ddfb] text-sm w-20 text-center ;">
               cleaning
             </div>
             <div className="businessTag"></div>
-            <h3 className="text-3xl font-roboto flex items-start justify-start gap-[80px] my-4;">House Cleaning</h3>
+            <h3 className="text-3xl font-roboto flex items-start justify-start my-5 gap-5 font-bold">House Cleaning</h3>
             <p className="text-[#a8a8a8] font-roboto text-xl my-1">255 Grand Park Ave, New York</p>
-            <p className="text-[#a8a8a8] font-roboto text-xl my-1">accounts@tubeguruji.com</p>
+            <p className="text-[#a8a8a8] font-roboto text-xl my-1 mb-20">accounts@tubeguruji.com</p>
           </div>
         </div>
         <div className="flex justify-end items-end flex-col gap-4">
@@ -34,10 +34,10 @@ export const BusinessPage = () => {
             Box
           </button>
           <p className="text-[#8056eb] font-roboto text-lg;">Jenny Wilson</p>
-          <p className="text-[#a8a8a8] font-roboto text-xl my-1;">Available 8:00 AM to 10PM</p>
+          <p className="text-[#a8a8a8] font-roboto text-xl mb-24 ;">Available 8:00 AM to 10PM</p>
         </div>
-        <div className="flex font-roboto items-start flex-col">
-          <h4 className="font-roboto text-xl font-bold ">Description</h4>
+        <div className="flex font-roboto items-start flex-col gap-5 bg-gradient-to-b from-white to-gray-200 h-full w-full pl-10">
+          <h4 className="font-roboto text-2xl font-bold ">Description</h4>
           <div className="text-left text-[#2e2929] font-roboto text-xl tracking-wide leading-relaxed w-11/12;">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla provident ipsum placeat aspernatur est
             accusamus architecto modi deleniti, ipsa vero voluptatem assumenda beatae vel illo quibusdam facere tenetur
@@ -46,20 +46,20 @@ export const BusinessPage = () => {
             quas fuga! Animi, voluptatibus.
           </div>
         </div>
-        <div>
+        <div className="flex flex-col gap-5  bg-gradient-to-b from-white to-gray-200 h-full w-full pl-5 pt-5 pr-10">
           <Button simple long onClick={openModal}>
             <SlNote className="text-2xl" />
             Book Appointment
           </Button>
-          <h4 className="text-left font-roboto text-xl;">Similar Business</h4>
-          <div className="simBusiness">
-            <div className="simBusinessCard">
-              <div className="flex justify-center items-start text-left gap-4;">
+          <h4 className="text-left font-roboto font-bold text-l ;">Similar Business</h4>
+          <div className="flex justify-start ">
+            <div className="">
+              <div className="flex gap-5  justify-center items-start text-left ;">
                 <img
                   src="https://www.pristinehome.com.au/wp-content/uploads/2020/01/15-Cleaning-Tips-from-Professional-Cleaners-3.jpg"
-                  className="w-20 h-25 object-cover rounded-md;"
+                  className="w-20 h-28 rounded-2xl object-cover ;"
                 />
-                <div className="infoBook">
+                <div className="flex flex-col ">
                   <h3 className="font-roboto text-xs;">House Cleaning</h3>
                   <p
                     className="font-roboto text-xs;
