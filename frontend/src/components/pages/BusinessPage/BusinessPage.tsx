cb2f0d76d9fc2e7990442ec97e68f1a8e9fe7a31
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-09-03 18:08:01 +0300

    icons added

--- frontend/src/components/pages/BusinessPage/BusinessPage.tsx
+++ frontend/src/components/pages/BusinessPage/BusinessPage.tsx
@@ -2,6 +2,10 @@
 import { useState } from 'react';
 import { Button } from '@/components/common/buttons/Button';
 import { SlNote } from 'react-icons/sl';
+import { FiMapPin } from 'react-icons/fi';
+import { HiOutlineMail } from 'react-icons/hi';
+import { LuUpload } from 'react-icons/lu';
+import { IoPersonOutline } from 'react-icons/io5';
 import { BookingCalendar } from '@/components/BookingCalendar/BookingCalendar';
 
 export const BusinessPage = () => {
@@ -25,15 +29,24 @@ export const BusinessPage = () => {
             </div>
             <div className="businessTag"></div>
             <h3 className="text-3xl font-roboto flex items-start justify-start my-5 gap-5 font-bold">House Cleaning</h3>
-            <p className="text-[#a8a8a8] font-roboto text-xl my-1">255 Grand Park Ave, New York</p>
-            <p className="text-[#a8a8a8] font-roboto text-xl my-1 mb-20">accounts@tubeguruji.com</p>
+            <p className="text-[#a8a8a8] font-roboto text-xl my-1 flex gap-2">
+              <FiMapPin />
+              255 Grand Park Ave, New York
+            </p>
+            <p className="text-[#a8a8a8] font-roboto text-xl my-1 mb-20 flex gap-2 ">
+              <HiOutlineMail />
+              accounts@tubeguruji.com
+            </p>
           </div>
         </div>
         <div className="flex justify-end items-end flex-col gap-4">
-          <button className="w-14 h-11 text-white bg-[#8056eb] rounded-lg border-none hover:bg-[#9270e7] cursor-pointer">
-            Box
-          </button>
-          <p className="text-[#8056eb] font-roboto text-lg;">Jenny Wilson</p>
+          <div className="w-14 h-11 text-white bg-[#8056eb] rounded-lg border-none hover:bg-[#9270e7] cursor-pointer ">
+            <LuUpload className="flex align-middle justify-center w-8 h-8 pt-1 ml-3  " />
+          </div>
+          <p className="text-[#8056eb] font-roboto text-lg flex flex-row align-middle justify-center;">
+            <IoPersonOutline className="flex w-4 h-4 m-2" />
+            Jenny Wilson
+          </p>
           <p className="text-[#a8a8a8] font-roboto text-xl mb-24 ;">Available 8:00 AM to 10PM</p>
         </div>
         <div className="flex font-roboto items-start flex-col gap-5 bg-gradient-to-b from-white to-gray-200 h-full w-full pl-10">
