3564c910434bf893243de874417ca77b46e759c7
Qetsya <m.nicole1994@gmail.com>
2024-09-11 21:35:46 +0300

    deployment fix

--- frontend/src/components/pages/HomePage/HomePage.tsx
+++ frontend/src/components/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import styles from './HomePage.module.css';
 import { Hero } from './Hero';
-import { CategoryList } from '@/components/Category/CategoryList';
+import { CategoryList } from '../../Category/CategoryList';
 import { BusinessList } from '../../Business/BusinessList';
 
 export const HomePage = () => {
